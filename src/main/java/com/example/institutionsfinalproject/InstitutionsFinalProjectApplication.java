484283c592dc4f917fee7880788500c2f7f26190
User <doronyuk714@mail.com>
2025-09-17 22:50:25 +0300

    Init commit

---
+++ src/main/java/com/example/institutionsfinalproject/InstitutionsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.institutionsfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutionsFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstitutionsFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/institutionsfinalproject/InstitutionsFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.institutionsfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutionsFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
