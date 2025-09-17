563c9ec9cfd55ca67a952ea5c51231501c0c56f4
User <doronyuk714@mail.com>
2025-09-17 23:04:11 +0300

    Init commit

---
+++ src/main/java/com/example/institutionsfinalproject/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package com.example.institutionsfinalproject.entity;
+
+public class UserEntity {
+    private Long id;
+    private String name;
+    private String surname;
+    private String address;
+    private Long age;
+
+}
