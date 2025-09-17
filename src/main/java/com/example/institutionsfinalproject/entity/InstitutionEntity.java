dec86c7108b4a4a01090edf60352df43ba672fa6
User <doronyuk714@mail.com>
2025-09-17 22:58:04 +0300

    Init commit

--- src/main/java/com/example/institutionsfinalproject/entity/InstitutionEntity.java
+++ src/main/java/com/example/institutionsfinalproject/entity/InstitutionEntity.java
@@ -2,4 +2,11 @@ package com.example.institutionsfinalproject.entity;
 
 
 public class InstitutionEntity {
+
+//    @Id
+    private Long id;
+    private String name;
+    private String photo;
+    private String workTime;
+    private double averageCheck;
 }
