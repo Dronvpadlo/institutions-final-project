5ee686456540689cb82f6ad1a33ce8d020384b7a
User <doronyuk714@mail.com>
2025-12-15 11:52:20 +0200

    Added Base validation

--- src/main/java/com/example/institutionsfinalproject/mapper/ReviewMapper.java
+++ src/main/java/com/example/institutionsfinalproject/mapper/ReviewMapper.java
@@ -3,9 +3,11 @@ package com.example.institutionsfinalproject.mapper;
 import com.example.institutionsfinalproject.entity.ReviewEntity;
 import com.example.institutionsfinalproject.entity.dto.ReviewDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
+    @Mapping(source = "rating", target = "rating")
     ReviewDTO toDto (ReviewEntity reviewEntity);
     ReviewEntity toEntity (ReviewDTO reviewDTO);
 }
